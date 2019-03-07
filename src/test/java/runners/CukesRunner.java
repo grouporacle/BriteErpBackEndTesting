be67ae9e1a9950961d68315a5c4e2b490d7e91ab
grouporacle <siivriisinek@gmail.com>
2019-03-07 11:00:55 -0600

    initial commit

---
+++ src/test/java/runners/CukesRunner.java
@@ -0,0 +1,25 @@
+package runners;
+
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+             "html:target/default-cucumber-reports",
+              "json:target/cucumber.json"
+        },
+        features = {"src/test/resources/features"},
+        glue = "com/BriteErp/step_definitions"
+        , dryRun = false
+        , tags = "@wip"
+)
+public class CukesRunner {
+
+
+}
